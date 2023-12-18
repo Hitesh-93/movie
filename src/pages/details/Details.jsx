a8f1e7c8f5228bacc8df6479add4f974ee0d9524
unknown <hiteshdahale@gmail.com>
2023-12-18 18:13:59 +0530

    first commit

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Details.scss'
 
+import { useParams } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch'
+import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
+import VideosSection from './videoSection/VideosSection'
+
+
 
 const Details = () => {
+
+    const { mediaType, id } = useParams();
+    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+
+
     return (
-        <div>Details</div>
+        <div>
+            <DetailsBanner video={data?.result?.[0]} crew={credits?.crew} />
+            <Cast data={credits?.cast} loading={creditsLoading} />
+            <VideosSection data={data} loading={loading} />
+        </div>
     )
 }
 
